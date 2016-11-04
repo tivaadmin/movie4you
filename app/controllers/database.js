27c3bd3cd3115153ce0eee690f9b5002149e1bb1
tivaadmin <tivateam2016@gmail.com>
2016-11-04 22:19:50 +0630

    .

--- app/controllers/database.js
+++ app/controllers/database.js
@@ -38,7 +38,7 @@ connection.connect(function (err) {
 
 // insert user
 exports.add_user = function (req, res) {
-    var query = connection.query("Insert into user set ?", req.body, function (req, res) {
+    var query = connection.query({sql: "Insert into user set ?", timeout: 40000}, req.body, function (req, res) {
         if (err) {
             return res.status(400).send(err);
         }
@@ -67,7 +67,7 @@ exports.get_popular = function (req, res) {
 // select all popular
 exports.get_allpopular = function (req, res) {
 
-    var query = connection.query('select distinct m.* from Movie m join Movie_Genre g on m.movieId = g.movieId join Register r on r.genreId = g.genreId where r.userId = 1 order by vote_average DESC,release_date DESC limit 0,100'
+    var query = connection.query({sql: 'select distinct m.* from Movie m join Movie_Genre g on m.movieId = g.movieId join Register r on r.genreId = g.genreId where r.userId = 1 order by vote_average DESC,release_date DESC limit 0,100', timeout: 40000}
         , function (err, result) {
 
             // console.log(req.body);
@@ -86,7 +86,9 @@ exports.get_allpopular = function (req, res) {
 // select movie detail
 exports.get_detail = function (req, res) {
 
-    var query = connection.query('select * from Movie where movieId = ?', req.params.movieId, function (err, result) {
+    var query = connection.query({sql: 'select * from Movie where movieId = ?', timeout: 40000}, req.params.movieId
+        , function (err, result) {
+
         if (err) {
             return res.status(400).send(err);
         } else {
@@ -103,7 +105,8 @@ exports.get_detail = function (req, res) {
 //select genre
 exports.get_genre = function (req, res) {
 
-    var query = connection.query('select g.genreName from Genre g join Movie_Genre mg on g.genreId = mg.genreId where mg.movieId = ?', req.params.movieId, function (err, result) {
+    var query = connection.query({sql: 'select g.genreName from Genre g join Movie_Genre mg on g.genreId = mg.genreId where mg.movieId = ?', timeout: 40000}, req.params.movieId
+        , function (err, result) {
         if (err) {
             return res.status(400).send(err);
         } else {
@@ -120,7 +123,7 @@ exports.get_genre = function (req, res) {
 // Insert Recommendation
 exports.add_recommendations = function (req, res) {
 
-    var query = connection.query('insert ignore into Recommendation set ?', req.body, function (err, result) {
+    var query = connection.query({sql: 'insert ignore into Recommendation set ?', timeout: 40000}, req.body, function (err, result) {
         if (err) {
             return res.status(400).send(err);
         } else {
@@ -136,7 +139,7 @@ exports.add_recommendations = function (req, res) {
 // Insert Like Movies
 exports.add_likeMovies = function (req, res) {
 
-    var query = connection.query('insert ignore into Like_Movies set ?', req.body, function (err, result) {
+    var query = connection.query({sql: 'insert ignore into Like_Movies set ?', timeout: 40000}, req.body, function (err, result) {
         if (err) {
             return res.status(400).send(err);
         } else {
@@ -152,7 +155,7 @@ exports.add_likeMovies = function (req, res) {
 // Insert Like Genre
 exports.add_likeGenre = function (req, res) {
 
-    var query = connection.query('insert ignore into Like_Genre set ?', req.body, function (err, result) {
+    var query = connection.query({sql: 'insert ignore into Like_Genre set ?', timeout: 40000}, req.body, function (err, result) {
         if (err) {
             return res.status(400).send(err);
         } else {
